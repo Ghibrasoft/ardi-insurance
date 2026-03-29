5482419e18bad7ef8bf458b11636081b441e7914
David <dgibradze@gmail.com>
2026-03-29 12:58:24 +0400

    refactor: vite cfg and indexcss

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,13 +2,11 @@ import Logo from "../common/logo";
 
 export function Header() {
   return (
-    <header className="bg-white border-b border-gray-200 sticky shadow-sm top-0 z-50">
+    <header className="bg-white border-b border-gray-200 sticky top-0 z-50 shadow-sm">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-3">
+        <div className="flex items-center">
           <Logo width={50} height={50} className="text-[#077a61]" />
         </div>
-
-        <div>theme switcher here...</div>
       </div>
     </header>
   );
